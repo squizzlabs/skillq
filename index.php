431cbe3eebc08c9339581c393c25558a97d2b1f7
cvweiss <cvweiss@gmail.com>
2025-12-21 12:41:22 +0000

    Fixes #70

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+if (!function_exists('get_magic_quotes_gpc')) {
+    function get_magic_quotes_gpc() {
+        return false;
+    }
+}
+
+
 $t = microtime(true);
 
 // Fire up the session!
